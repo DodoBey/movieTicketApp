412997718bac837d951ff0ff066e8a098cf0f4f3
Dogukan Yigiter <77416196+DodoBey@users.noreply.github.com>
2021-04-30 02:36:25 -0700

    Add files via upload

---
+++ my-cinema/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieList from './Components/MovieList';
+import MoviesComponent from './Components/MoviesComponent';
+
+function App() {
+  return (
+    <div className="all">
+    <MoviesComponent/>
+    </div>
+  );
+}
+
+export default App;
