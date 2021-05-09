f2feeb69011c19975a4ccadf8c0e564a3a787618
Dogukan Yigiter <77416196+DodoBey@users.noreply.github.com>
2021-05-08 21:01:15 -0700

    Add files via upload

--- my-cinema/src/Components/Modal.js
+++ my-cinema/src/Components/Modal.js
@@ -4,18 +4,22 @@ import './Modal.css'
 
 const Modal = (props) => props.isShowing ? ReactDOM.createPortal(
   <>
-    <div style={{ background: `url(${props.image})`}} onClick={props.isShowing} className="allModal">
-      <div className="leftGrid">
-        <h3>{props.title}</h3>
-        <h6 id={props.genresId}>{props.genres}</h6>
-        <span id={props.starringId}>Starring: {props.starring}</span>
-        <span>Duration: {props.duration}</span>
-        <span>Release Date: {props.release}</span>
-        <span>MovieDB Rate: {props.rate}</span>
-      </div>
-      <div className="rightGrid">
-        <div><iframe width="100%" height="300vw" src={props.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-        <p>{props.sum}</p>
+    <div className="bgModal" onClick={props.hide}>
+      <div style={{ background: `url(${props.image})` }} className="allModal">
+        <div className="title">
+          <h3>{props.title}</h3>
+          <h6 id={props.genresId}>{props.genres}</h6>
+        </div>
+        <div>
+          <iframe width="100%" height="300vw" src={props.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+        <div>
+          <p>{props.sum}</p>
+          <span id={props.starringId}>Starring: {props.starring}</span>
+          <span>Duration: {props.duration}</span>
+          <span>Release Date: {props.release}</span>
+          <span>MovieDB Rate: {props.rate}</span>
+        </div>
         <div>
           <a href={props.imdbId} target="_blank">
             <button>
